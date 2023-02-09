611524f4fbbe4221134e050b13df5cebb9af252e
Matee <matiullahsafibbc@gmail.com>
2023-02-09 15:18:13 -0600

    Fix linter errors

--- menuFunction.js
+++ menuFunction.js
@@ -5,12 +5,12 @@ const closeCross = document.querySelectorAll('.close-cross');
 function toggleMenu() {
   document.querySelectorAll('.nav-items').forEach((element) => {
     element.classList.toggle('open-nav');
-  })
+  });
 }
 
 hamburgerButton.forEach((element) => {
-    element.addEventListener('click', toggleMenu);
-})
+  element.addEventListener('click', toggleMenu);
+});
 closeCross.forEach((element) => {
-    element.addEventListener('click', toggleMenu);
-})
+  element.addEventListener('click', toggleMenu);
+});
